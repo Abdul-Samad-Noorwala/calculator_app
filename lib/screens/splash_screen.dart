7c3a3beb93a30bfb2d2a40aa1d2ed454124fdd2a
Abdul Samad <145452813+Abdul-Samad-Noorwala@users.noreply.github.com>
2024-05-17 15:18:44 +0500

    Created Splash Screen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,50 @@
+import 'dart:async';
+
+import 'package:calculator_app/screens/calculator_screen.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const CalculatorScreen(),
+          ));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              height: 300,
+              "assets/calculator.png",
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              "Calculator App",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
